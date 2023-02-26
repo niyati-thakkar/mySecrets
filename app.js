724ed89d67a766faf75bbd5f2d31880cd9196a2c
Your Name <you@example.com>
2023-02-26 12:05:23 +0530

    updated

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "https://mysecrets.onrender.com//auth/google/secrets",
+  callbackURL: "https://mysecrets.onrender.com/auth/google/secrets",
   // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb) {
